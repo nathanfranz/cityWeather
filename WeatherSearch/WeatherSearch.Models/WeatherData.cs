431d7f883efecc87e6764b00800c5caa9fa38049
Nathan Franz <nathancfranz@gmail.com>
2020-10-30 13:52:49 -0400

    initial commit

---
+++ WeatherSearch/WeatherSearch.Models/WeatherData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WeatherSearch.Models
+{
+    public class WeatherData
+    {
+        public Main main { get; set; }
+        
+        public Wind wind { get; set; } 
+    }
+
+    public class Main
+    {
+        public string temp { get; set; }
+        
+        public string humidity { get; set; }
+    }
+    
+    public class Wind
+    {
+        public string speed { get; set; }
+        
+        public string deg { get; set; }
+    }
+}
---
+++ WeatherSearch/WeatherSearch/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherSearch.DL.Repositories;
+using WeatherSearch.Models;
+
+namespace WeatherSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(string city)
+        {
+            if (!string.IsNullOrEmpty(city))
+            {
+                var weather = WeatherSearchRepository.GetWeatherDataFromCity(city);
+
+                if (weather.main != null)
+                {
+                    ViewData["CityWeather"] = city + " Weather Data:";
+                    ViewData["Temperature"] = "Temperature: " + weather.main.temp + " Kelvin";
+                    ViewData["Humidity"] = "Humidity: " + weather.main.humidity + "%";
+                    ViewData["WindSpeed"] = "Wind Speed: " + weather.wind.speed + " meter/sec";
+                    ViewData["WindDirection"] = "Wind Direction: " + weather.wind.speed + " degrees";
+                }
+                else
+                {
+                    ViewData["NoWeatherData"] = "No weather data was found for the city: " + city;
+                }
+            }
+
+            return View();
+        }
+    }
+}
